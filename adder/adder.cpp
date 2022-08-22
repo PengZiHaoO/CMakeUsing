63ed35193119f48ca161b9d8ef8fb9e1e0e5624e
Your Name <2039748459@qq.com>
2022-08-22 18:56:20 +0800

    first commit

---
+++ adder/adder.cpp
@@ -0,0 +1,5 @@
+#include "adder.h"
+
+double add(double a, double b){
+    return (a + b);
+}
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "adder.h"
+
+int main(){
+    std::cout << "hello,there\n";
+    
+    std::cout << add(74.2, 56.6) << '\n';
+
+    return 0;
+}
