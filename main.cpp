19020262ae535a058c02c97a76c8f880a93cf172
Your Name <2039748459@qq.com>
2022-08-22 23:17:02 +0800

    auto push

--- main.cpp
+++ main.cpp
@@ -1,10 +1,41 @@
 #include <iostream>
 #include "adder.h"
+#include "GLFW/glfw3.h"
 
 int main(){
     std::cout << "hello,there\n";
     
     std::cout << add(74.2, 56.6) << '\n';
+
+    GLFWwindow* window;
+    int width, height;
+
+    if( !glfwInit() )
+    {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        exit( EXIT_FAILURE );
+    }
+
+    glfwWindowHint(GLFW_DEPTH_BITS, 16);
+    glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
+
+    window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
+    if (!window)
+    {
+        fprintf( stderr, "Failed to open GLFW window\n" );
+        glfwTerminate();
+        exit( EXIT_FAILURE );
+    }
+
+     while( !glfwWindowShouldClose(window) ){
+
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Terminate GLFW
+    glfwTerminate();
 
     return 0;
 }
